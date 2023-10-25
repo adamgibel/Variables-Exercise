b8990d90f5f45dca5218b3ece247051b40e36839
adam gibel <adamgibel@gmail.com>
2023-10-24 20:59:24 -0400

    declaring and initializing variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿// Declaring and initializing variables 
+string Name = "Adam";
+int Age = 31;
+char Middleinitial = 'R';
+bool Isover18 = true;
+double CurrentTime = 8;
+decimal CurrentTemp = 66.0M;
+
+// Writing the variables into the console
+Console.WriteLine($"Name: {Name}");
+Console.WriteLine($"Age: {Age}");
+Console.WriteLine($"Middleinitial: {Middleinitial}");
+Console.WriteLine($"Isover18: {Isover18}");
+Console.WriteLine($"CurrentTime: {CurrentTime}");
+Console.WriteLine($"CurrentTemp: {CurrentTemp}");
